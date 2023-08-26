78fc1e2f6ab0e6f1ff2752a9a65fd440d1ada830
Lijazilla <alejandro-1986-jf@hotmail.com>
2023-08-26 15:16:10 -0600

    se modificó el index para dejar solo la app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import { BrowserRouter as Router } from "react-router-dom";
 // import MainRouter from "./mainRouter"; 
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
